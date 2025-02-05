2e8be7d160f0f644858cf4202567ffdbf1bcbf98
Mahé Perrette <mahe.perrette@gmail.com>
2025-02-05 16:16:42 +0100

    first working-version

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # Datasets
-Keep track of datasets used in a project
+
+Keep track of datasets used in a project.
+
+Provide a simple and straightforward way to keep track of datasets downloaded from the web.
+
+Currently Datasets supports download from a set of URLs suited for repositories like PANGEA or ZENODO, as well as git-based repositories such as github. Support for more remote repositories will be added along the way as necessary.
+
+It provides declarative functions to register and download datasets, as well as a way to write to and read from an equivalent (and optional) `toml` config file.
+
+## Examples
+
+Examples of the declarative syntax
+```julia
+
+import Datasets: register_dataset, register_repository, DATASETS
+
+herzschuh2021 = register_dataset("10.1594/PANGAEA.930512";
+    downloads=["https://doi.pangaea.de/10.1594/PANGAEA.930512?format=zip"],
+    )
+
+jonkers2024 = register_dataset("10.1594/PANGAEA.962852";
+    downloads=["https://download.pangaea.de/dataset/962852/files/LGM_foraminifera_assemblages_20240110.csv"],
+    )
+
+tierney2020 = register_repository("git@github.com:jesstierney/lgmDA.git")
+
+println(DATASETS)
+```
+yields:
+```julia
+```
+
+An equivalent declaration can be defined in a toml file for more clarify (that's what I'd recommended):
+
+```toml
+[herzschuh2021]
+downloads = ["https://doi.pangaea.de/10.1594/PANGAEA.930512?format=zip"]
+doi = "10.1594/PANGAEA.930512"
+
+[jonkers2024]
+downloads = ["https://download.pangaea.de/dataset/962852/files/LGM_foraminifera_assemblages_20240110.csv"]
+doi = "10.1594/PANGAEA.962852"
+
+[tierney2020]
+remote = "git@github.com:jesstierney/lgmDA.git"
+```
+
+Note dataset-specific parameters can be provided to overwrite global parameters such as `datasets_path`.
+
+And read via the `register_datasets` function
+
+```julia
+
+import Datasets: save_datasets
+datasets = register_datasets("config.yml")
+```
+
+At present there is a global variable `DATASETS` that contains all datasets,
+so that is it not needed as an input argument to the functions, but
+optional parameters make it possible to handle separate dataset files.
+
+Finally, the datasets can be downloaded straightforwardly:
+
+```julia
+import Datasets: download_dataset, download_datasets
+
+download_dataset("jonkers2024")
+download_datasets()  # download all datasets defined in DATASETS
+```
+
+That files will be downloaded to their `folder` key, which defaults to a local `datasets` folder,
+and will then use their DOI if provided, or the github remote, or their name / alias otherwise.
+
+Details are still being worked out.
+
+
+## Why Datasets.jl ?
+
+It seems there are quite a few tools to help project and data management. What I stumbled upon includes [Dr Watson](https://juliadynamics.github.io/DrWatson.jl/dev/), [DataToolKit.jl](https://discourse.julialang.org/t/ann-datatoolkit-jl-reproducible-flexible-and-convenient-data-management/104757) and [RemoteFiles.jl](https://github.com/helgee/RemoteFiles.jl). RemoteFiles.jl does not provide enough documentation for me to judge at this stage. **Dr Watson** aims at assisting with all aspects of how to organize files in a scientific project, including running simulations etc, and as such it has a broader scope than **Datasets.jl**. **DataToolKit.jl** is the only package I actually tried. What I can say is it is impressive because it merges apparent simplicity of use depth of functionality. I'd say if Datasets.jl ever attempts to get past the download and on-disk management of datasets, with things like actual data loaders including lazy loading of web ressources, it should probably stop right there and use DataToolKits instead.
+
+What made me publish this package instead of just relying on DataTookKit.jl is the KISS principle (Keep It Simple & Stupid). I dislike the idea of having data loaders included as this massively overburdens the core functionality (keep track of things), and examples provided in DataToolKit to clean-up datasets were not convincing to me: too much is kept hidden with ugly meta `@syntax` mixed in the config files, were normal functions could do the job. Also I found it not straightforward to use the files as they are downloaded (thinking about a zip file that contained CSV data in need of custom loading) and it was not immediately clear to me how to store files on disk (it might be possible though!). Anyway, the **DataToolKit.jl** project is very good and has a dedicated main developer giving talks and it will evolve and you should check it out! For now though, **Datasets.jl** is so simple and tiny that it can be useful for whoever wants to follow the KISS principle.
---
+++ src/Datasets.jl
@@ -0,0 +1,240 @@
+module Datasets
+
+import TOML
+
+export DATASETS_PATH, DATASETS, register_dataset, register_repository, search_datasets, search_dataset, download_dataset, download_datasets, register_datasets, write_datasets_toml, register_datasets_toml
+
+DATASETS_PATH = "datasets"
+DATASETS = Dict()
+COMPRESSED_FORMATS = ["zip", "tar.gz", "tar"]
+EXTRACT = true
+
+function register_dataset(name::String=""; doi::Union{Nothing,String}=nothing,
+    aliases::Vector{String}=Vector{String}(), downloads::Vector{String}=Vector{String}(),
+    folder=nothing, datasets=DATASETS, overwrite::Bool=false)
+    if (name == "" && doi !== nothing)
+        name = doi
+    end
+    if name == ""
+        error("name or doi must be provided")
+    end
+    if haskey(datasets, name) && !overwrite
+        error("Dataset $name already exists. Set overwrite=true to overwrite.")
+    end
+    datasets[name] = Dict(
+        "doi" => doi,
+        "downloads" => downloads,
+        "folder" => joinpath(folder === nothing ? DATASETS_PATH : folder, doi === nothing ? name : doi)
+    )
+    if length(aliases) > 0
+        datasets[name]["aliases"] = aliases
+    end
+    return datasets[name]
+end
+
+
+"""
+parse git@github.com:awi-esc/Datasets.git OR https://github.com/awi-esc/Datasets.git
+as server=github.com, group=awi-esc, repo=Datasets
+"""
+function _parse_git_remote(remote::String)
+    if startswith(remote, "git@")
+        server, group_repo = split(remote[length("git@")+1:end], ":")
+    elseif startswith(remote, "https://")
+        remote = remote[length("https://")+1:end]
+        server = split(remote, "/")[1]
+        group_repo = remote[length(server)+2:end]
+    else
+        error("Unknown remote type: $remote . Expected git@ or https://")
+    end
+    group, repo = split(group_repo, "/")
+    if endswith(repo, ".git")
+        repo = repo[1:end-4]
+    end
+    return Dict("server" => server, "group" => group, "repo" => repo)
+end
+
+
+function register_repository(name::String, remote::String; folder=nothing, type="git", datasets=DATASETS, overwrite::Bool=false)
+    if name == ""
+        name, _ = splitext(basename(remote))
+    end
+    if haskey(datasets, name) && !overwrite
+        error("Dataset $name already exists. Set overwrite=true to overwrite.")
+    end
+    parsed = _parse_git_remote(remote)
+    datasets[name] = Dict(
+        "remote" => remote,
+        "folder" => joinpath(folder === nothing ? DATASETS_PATH : folder, parsed["server"], parsed["group"], parsed["repo"]),
+        "type" => type,
+        "aliases" => [joinpath(parsed["group"], parsed["repo"]), parsed["repo"]],
+    )
+    return datasets[name]
+end
+
+function register_repository(remote::String; name::String="", kwargs...)
+    return register_repository(name, remote; kwargs...)
+end
+
+function extract_file(download_path)
+    download_dir = dirname(download_path)
+    if endswith(download_path, ".zip") || occursin("?format=zip", download_path)
+        run(`unzip -o $download_path -d $download_dir`)
+    elseif endswith(download_path, ".tar.gz")
+        run(`tar -xzf $download_path -C $download_dir`)
+    elseif endswith(download_path, ".tar")
+        run(`tar -xf $download_path -C $download_dir`)
+    else
+        error("Unknown file type: $download_path")
+    end
+end
+
+function search_datasets(name; datasets=DATASETS, exact=false)
+
+    exact_results = []
+    partial_results = []
+
+    # first check exact matches in keys
+    if haskey(datasets, name)
+        push!(exact_results, datasets[name])
+    end
+
+    # then check exact matches in alias or doi
+    for (key, dataset) in pairs(datasets)
+        if (haskey(dataset, "aliases") && (name in dataset["aliases"]))
+            push!(exact_results, dataset)
+        elseif haskey(dataset, "doi") && dataset["doi"] == name
+            push!(exact_results, dataset)
+        end
+    end
+
+    if exact
+        return exact_results
+    end
+
+    # then check partial matches in keys
+    for key in keys(datasets)
+        if name != key && occursin(name, key)
+            push!(partial_results, datasets[key])
+        end
+    end
+
+    # then check partial matches in alias
+    for (key, dataset) in pairs(datasets)
+        if haskey(dataset, "aliases") && any(x -> occursin(name, x), dataset["aliases"])
+            push!(partial_results, dataset)
+        elseif haskey(dataset, "doi") && name != dataset["doi"] && occursin(name, dataset["doi"])
+            push!(partial_results, dataset)
+        end
+    end
+
+    return unique(vcat(exact_results, partial_results))
+end
+
+function search_dataset(name; datasets=DATASETS, exact=false, check_unique=false, raise=true)
+    results = search_datasets(name; datasets=datasets, exact=exact)
+    if length(results) == 0
+        error("No dataset found for $name")
+    elseif (check_unique && length(results) > 1)
+        message = "Multiple datasets found for $name: $(join([get(x, "doi", x) for x in results], ", "))"
+        if raise
+            error(message)
+        else
+            warn(message)
+        end
+    end
+    return results[1]
+end
+
+
+function download_dataset(name; extract=nothing, datasets=DATASETS)
+    if ! haskey(datasets, name)
+        dataset = search_dataset(name, exact=true)
+    end
+    dataset = datasets[name]
+    download_dir = dataset["folder"]
+
+    if haskey(dataset, "type")
+        if dataset["type"] == "git"
+            if !isdir(joinpath(download_dir, ".git"))
+                run(`git clone $(dataset["remote"]) $download_dir`)
+            end
+            return
+        end
+    end
+
+    if !isdir(download_dir)
+        mkpath(download_dir)
+    end
+    for download in dataset["downloads"]
+        download_path = joinpath(download_dir, basename(download))
+        if !isfile(download_path)
+            cd(() -> run(`wget $download`), download_dir)
+            if (extract === nothing ? EXTRACT : extract) && any(endswith(download_path, formats) for formats in COMPRESSED_FORMATS)
+                extract_file(download_path)
+            end
+        end
+    end
+end
+
+function download_datasets(names=nothing; extract=nothing)
+    if names === nothing
+        names = keys(DATASETS)
+    end
+    for name in names
+        download_dataset(name; extract=extract)
+    end
+end
+
+function register_datasets(datasets::Dict; datasets_path::Union{Nothing,String}=nothing, kwargs...)
+    if datasets_path === nothing
+        datasets_path = DATASETS_PATH
+    end
+
+    for (name, info) in pairs(datasets)
+        if haskey(info, "remote")
+            register_repository(name, info["remote"]; folder=get(info, "folder", datasets_path), type=get(info, "type", "git"), kwargs...)
+        else
+            register_dataset(name ;doi=info["doi"], downloads=info["downloads"], folder=get(info, "folder", datasets_path), kwargs...)
+        end
+    end
+end
+
+function register_datasets_toml(filepath; kwargs...)
+    config = TOML.parsefile(filepath)
+    register_datasets(config; kwargs...)
+end
+
+function _clean_datasets(datasets::Dict=DATASETS)
+    datasets_clean = Dict()
+    for (name, info) in pairs(datasets)
+        datasets_clean[name] = Dict()
+        for (key, value) in pairs(info)
+            if (key == "folder")
+                continue
+            end
+            if value !== nothing && value !== [] && value !== Dict() && value !== ""
+                datasets_clean[name][key] = value
+            end
+        end
+    end
+    return datasets_clean
+end
+
+function write_datasets_toml(filepath, datasets::Dict=DATASETS)
+    datasets_clean = _clean_datasets(datasets)
+    open(filepath, "w") do io
+        TOML.print(io, datasets_clean)
+    end
+end
+
+function register_datasets(filepath::String; datasets_path::Union{Nothing,String}=nothing, kwargs...)
+    ext = splitext(filepath)[2]
+    if ext == ".toml"
+        register_datasets_toml(filepath; datasets_path=datasets_path, kwargs...)
+    else
+        error("Only toml file type supported. Got: $ext")
+    end
+end
+
+end # module
